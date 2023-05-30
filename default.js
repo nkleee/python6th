f4f16ff2ca17f86cba57d68f7d387c353b56612c
nkleee <nogi3001@gmail.com>
2023-05-30 13:15:36 +0900

    230529 공부내용 자바스크립트 변수

--- default.js
+++ default.js
@@ -1,12 +1,39 @@
+var name = "이승훈";
+var age = 29;
+var cgpa = 3.92;
+var lineBreak = "<br/>"
+
+document.write("이름: " + name + lineBreak);
+document.write("나이: " + age + lineBreak);
+document.write("학점: " + cgpa + lineBreak);
+
+
+
+//console.log("123 = " + typeof 123);
+//console.log("123.5 = " + typeof 123.5);
+//console.log('"123" = ' + typeof "123");
+//console.log("true = " + typeof true);
+//console.log("false = " + typeof false);
+//
+//var car;
+//console.log(typeof car);
+//var car = "";
+//console.log(typeof car);
+//
+//person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+//console.log(typeof person);
+//person = null;
+//console.log(typeof person);
+/*
 document.write("Hello World2");
 document.write("<h1>Welcome to JS Program</h1>");
 document.write("<h2>Welcome to JS Program</h2>");
-
-console.log("Welcome to JS Program");
-console.info("Welcome to JS Program");
-console.warn("Welcome to JS Program");
-console.error("Welcome to JS Program");
-
-alert("Welcome to JS Program");
-var a = prompt("Welcome to JS Program");
-console.log(a);
+*/
+//console.log("Welcome to JS Program");
+//console.info("Welcome to JS Program");
+//console.warn("Welcome to JS Program");
+//console.error("Welcome to JS Program");
+//
+//alert("Welcome to JS Program");
+//var a = prompt("Welcome to JS Program");
+//console.log(a);
