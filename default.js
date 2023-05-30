297f5555aa448a5b5e7b76d0a56e9dbffa2b48b7
nkleee <nogi3001@gmail.com>
2023-05-30 14:06:08 +0900

    230529 공부내용 자바스크립트 문자열 함수

--- default.js
+++ default.js
@@ -1,13 +1,54 @@
-var name = "이승훈";
-var age = 29;
-var cgpa = 3.92;
-var lineBreak = "<br/>"
+var text = prompt("Enter your name");
+if (text === null)
+{text = "";}
+document.write("Your name : " + text + "<br/>");
 
-document.write("이름: " + name + lineBreak);
-document.write("나이: " + age + lineBreak);
-document.write("학점: " + cgpa + lineBreak);
+var len = text.length;
+document.write("Number of characters : " + len + "<br/>");
+
+document.write(text.charAt(2) + "<br>");
+document.write(text.toUpperCase() + "<br>");
+document.write(text.toLowerCase() + "<br>");
+
+var text1 = "hi, ";
+var text2 = "bye";
+var text3 = text1.concat(text2);
+var text4 = text1 + text2;
+
+document.write(text3 + "<br/>");
+document.write(text4 + "<br/>");
+
+var text5 = "hello";
+var result = text5.slice(0,2);
+document.write(result + "<br/>");
 
 
+//var lastName = "홍";
+//var firstName = "길동";
+//
+//var fullName = lastName + firstName;
+//
+//console.log(fullName);
+//console.log("Today is" + " a " + "beautiful day");
+//console.log("My name is " + fullName);
+//
+//var num1 = 20;
+//var num2 = 30;
+//var sum = num1 + num2;
+//console.log(num1 + num2);
+//console.log("" + num1 + num2);
+//console.log(num1 + " + " + num2 + " = " + sum);
+
+
+
+//var name = "이승훈";
+//var age = 29;
+//var cgpa = 3.92;
+//var lineBreak = "<br/>"
+//
+//document.write("이름: " + name + lineBreak);
+//document.write("나이: " + age + lineBreak);
+//document.write("학점: " + cgpa + lineBreak);
 
 //console.log("123 = " + typeof 123);
 //console.log("123.5 = " + typeof 123.5);
