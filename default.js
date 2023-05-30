a5af7498751e764f75a9397d47f2ef5268ed80a0
nkleee <nogi3001@gmail.com>
2023-05-30 11:24:25 +0900

    230529 공부내용 자바스크립트 출력함수

--- default.js
+++ default.js
@@ -1,1 +1,12 @@
-document.write('Hello World2');
+document.write("Hello World2");
+document.write("<h1>Welcome to JS Program</h1>");
+document.write("<h2>Welcome to JS Program</h2>");
+
+console.log("Welcome to JS Program");
+console.info("Welcome to JS Program");
+console.warn("Welcome to JS Program");
+console.error("Welcome to JS Program");
+
+alert("Welcome to JS Program");
+var a = prompt("Welcome to JS Program");
+console.log(a);
