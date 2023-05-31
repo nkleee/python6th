bb0b0b5e7f7fbe6b3aca0dab1a33502f744e989f
nkleee <nogi3001@gmail.com>
2023-05-31 09:48:56 +0900

    230531 공부내용 자바스크립트 조건부 제어문

--- default.js
+++ default.js
@@ -1,9 +1,68 @@
-var base = parseFloat(prompt("밑변 입력 : "));
-var height = parseFloat(prompt("높이 입력 : "));
+var num1 = parseFloat(prompt("첫번째 숫자 입력 : "));
+var num2 = parseFloat(prompt("두번째 숫자 입력 : "));
 
-var area = base * height * 0.5;
+if(num1 > num2) {
+    console.log("큰 수는 num1 : " + num1);
+}
 
-document.write("삼각형의 넓이 : " + area);
+if(num1 < num2) {
+    console.log("큰 수는 num2 : " + num2);
+}
+
+if(num1 == num2) {
+    console.log("값이 동일합니다");
+}
+
+if(num1 > num2){
+    console.log("큰 수 num1: " + num1);
+} else if (num1 < num2) {
+    console.log("큰 수 num2: " + num2);
+} else if (num1 == num2) {
+    console.log("값이 동일합니다");
+}
+
+if(num1 > num2){
+    console.log("큰 수 num1: " + num1);
+} else if (num1 < num2) {
+    console.log("큰 수 num2: " + num2);
+} else (num1 == num2) {
+    console.log("값이 동일합니다");
+}
+
+//var num1 = 20;
+//var num2 = 10;
+//var num3 = "10";
+//var num4 = 20;
+//var num5 = 15;
+//console.log("일반 크기비교");
+//console.log(num1 > num2, num1, ">", num2);
+//console.log(num1 >= num2, num1, ">=", num2);
+//console.log(num1 < num2, num1, "<", num2);
+//console.log(num1 <= num2, num1, "<=", num2);
+//console.log("같은지 여부확인");
+//console.log(num1 == num4, num1, "==", num2);
+//console.log(num1 != num4, num1, "!=", num2);
+//console.log("===");
+//console.log(num1 === num3, num1, "===", num2);
+//console.log(num2 === num3, num1, "===", num2);
+//console.log(num2 == num3, num1, "==", num2);
+//
+//console.log("num1 > num2 && num1 > num5: " , num1 > num2 && !(num1 < num5));
+//console.log("num1 > num2 || num1 > num5: " , num1 > num2 || num1 < num5);
+
+
+//var cels = parseFloat(prompt("썹씨 입력: "));
+//var farn = cels * (9 / 5) + 32;
+//
+//document.write("화씨: " + farn);
+
+
+//var base = parseFloat(prompt("밑변 입력 : "));
+//var height = parseFloat(prompt("높이 입력 : "));
+//
+//var area = base * height * 0.5;
+//
+//document.write("삼각형의 넓이 : " + area);
 
 //var input1 = parseInt(prompt("Enter first number : "));
 //var num1 = parseInt(input1);
