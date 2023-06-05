6ca004a0cea85b7638b52dbe4123f998f3ae0b71
nkleee <nogi3001@gmail.com>
2023-06-05 15:23:45 +0900

    230605 파이썬 출력 함수

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+data = [10, 20, -50, 21.3, 'LikeLion']
+print(data)
+
+print("Like", "Share", "Subscribe", sep='')
+print("Like", "Share", "Subscribe", sep='***')
+
+print("Like", "Share", "Subscribe", sep='***', end='\t')
+print("Like", "Share", "Subscribe", sep='***', end='\n')
+
+m = 40
+print("value: ", m)
+
+name = "배정만"
+age = 42
+print("My name is", name, "and My age is", age, sep=' ')
+
+
+
 #줄바꿈 없이 출력
 print("1")
 print("2", end='')
