7871c6d2fb280017b5a2a25911f8b89a4cf4513b
nkleee <nogi3001@gmail.com>
2023-06-05 18:03:06 +0900

    230605 파이썬 출력 함수

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+# name = input()
+#
+# print(name)
+#
+# name = input("Your Name: ")
+mobile = input("Enter Your Mobile Number:")
+mb = int(mobile)
+print(mb, type(mb))
+
+#print(mb, type(mb))
+
+price = int(input("Total Price:"))
+
+print(price)
+complex_number = complex(input("Enter Complex Number:"))
+print(complex_number)
+
+print("He said, \"Hello World\"")
+print('He said, "Hello World"')
+print('It\'s beautiful day')
+print("It's beautiful day")
+
+
 data = [10, 20, -50, 21.3, 'LikeLion']
 print(data)
 
