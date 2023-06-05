c6a6510e0fb5c4b86fc109e68c37a8fce75ed191
nkleee <nogi3001@gmail.com>
2023-06-05 13:20:49 +0900

    230605 파이썬 논리 연산자

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# 논리 연산자 (4교시)
+a = 5
+b = 2
+c = 3
+d = 200
+
+print('AND 연산자')
+print('a > b and a > c:', a > b and a < c)
+
+print('OR 연산자')
+print('a > b and a > c:', a > b or a < c)
+
+print('NOT 연산자')
+print('not(a < b):', not(a < b))
+
+
 # 덧셈 (3교시)
 a = 4
 b = 2.0
