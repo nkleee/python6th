5d80b3487dc233a9e7e725eac0d35d8aa6c037bd
nkleee <nogi3001@gmail.com>
2023-06-05 14:14:51 +0900

    230605 파이썬 암시적 타입 변환

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+#암시적 타입 변환
+a = 5
+b = 2
+print(b, type(b))
+value = a / b
+print(value)
+print(type(value))
+
+x = 10
+y = 5.5
+total = x + y
+print(total)
+print(type(total))
+
+j = "Hello"
+k = "like lion"
+p = j + k
+print(p)
+print(type(p))
+
+q = 20
+u = '10'
+r = q + u
+print(r)
+
 # is 연산자
 a = 10
 b = 10
