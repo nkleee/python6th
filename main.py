1672531c09b7e89f2bc37b15f9dd245c3c9a1548
nkleee <nogi3001@gmail.com>
2023-06-05 11:30:53 +0900

    230605 파이썬 산술 연산자 실습

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-# 변수 선언
+# 덧셈 (3교시)
+a = 4
+b = 2.0
+total = a + b
+print('total = a + b:', total)
+
+# 뺄셈
+total = a - b
+print('total = a - b:', total)
+
+# 곱셈
+total = a * b
+print('total = a * b:', total)
+
+# 나눗셈
+print('a / b:', a / b)
+
+# 나머지
+a = 5.5
+b = 2.0
+print('a%b: ', a % b)
+
+# 거듭제곱
+print('a ** b: ', a ** b)
+
+# 몫 (양수)
+print('a // b: ', a // b)
+
+# 몫 (음수)
+a = -5
+print('a // b(음수) :', a // b)
+
+# 변수 선언 (2교시)
 a = 10
 b = 3.14
 c = "Hello, World!"
 d = [1, 2, 3]
 e = (4, 5, 6)
 f = {7, 8, 9}
-g = {"apple" : 1, "banana": 2, "orange": 3}
+g = {"apple": 1, "banana": 2, "orange": 3}
 
 # 데이터 타입 출력
 print("a: ", type(a))
