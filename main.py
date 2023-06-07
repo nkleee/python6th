b8a387bed1a626065614584c9afa0889cc93d36b
nkleee <nogi3001@gmail.com>
2023-06-07 10:20:45 +0900

    230607 while문

--- main.py
+++ main.py
@@ -1,3 +1,57 @@
+
+
+# 첫번째 while 루프
+a = 1
+while a <= 10:
+    print(a)
+    a += 1
+print("코드 종료")
+
+a = 2
+while a <= 20:
+    print(a)
+    a += 2
+print("코드 종료")
+
+a = 1
+while a <= 10:
+    print(a)
+    a += 1
+else:
+    print("While 조건이 거짓이므로 Else 부분 실행됨")
+print("코드 종료")
+
+# 무한 루프
+# while True:
+#     print("멋쟁이 사자")
+# print("코드 종료")
+
+i = 0
+while True:
+    i += 1
+    print(i)
+    if i == 5:
+        break
+print("코드 종료")
+
+# 중첩
+i = 1
+while i <= 3:
+    print("Outer Loop", i)
+    i += 1
+    j = 1
+    while j <= 5:
+        print("Inner Loop", j)
+        j += 1
+print("코드 종료")
+
+# i = 11
+# while(i < 10):
+#     i+= 1
+#     print("i: ",i)
+# else:
+#     print("else")
+
 # 예제 1: 간단한 if 문
 x = 5
 if x > 3:
