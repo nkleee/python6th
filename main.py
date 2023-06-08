d701ae16371556b3c15969e494c7d80e30a43e03
nkleee <nogi3001@gmail.com>
2023-06-08 10:15:11 +0900

    230608 위치 키워드 인수

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-def disp(sh):
-    print("Disp Function")
-    return sh
+def pw(x, y):
+    z = x ** y
+    print(z)
 
-def show():
-    return "Show Function"
+pw(2, 5)
+# pw(5, 2, 3) 2개의 값을 입력 받아야하는데 3개를 받아서 error
 
+def show(name, age=27):
+    print(f"Name: {name} Age:{age}")
 
-r_sh = disp(show)
-print(r_sh())
-print(show())
+show(name="멋쟁이사자", age=42)
+
+show(name="멋쟁이사자")
+
